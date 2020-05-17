214e8e16dbdbde64a9077ba375f5a6235939fb30
Alan Shum <alan200994@gmail.com>
2020-05-16 21:50:33 -0700

    set timeout to allow attaching to debugger

--- promise.ts
+++ promise.ts
@@ -3,8 +3,8 @@ const filePromise: Promise<Deno.File> = Deno.open('./import-map.json');
 filePromise.then((file: Deno.File) => {
   setTimeout(() => {
     debugger;
+    // need timeout to allow time to attach debugger in Chrome dev tool
+    // deno --inspect promise.ts
     console.log(file);
-    
   }, 5000)
-  console.log(file);
 });
